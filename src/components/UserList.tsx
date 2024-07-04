7ebb5b9f8a5e253f20492238f5a609ddb3df7db4
sameemul-haque <samhaqk@gmail.com>
2024-07-04 10:47:56 +0530

    lazy loading with scroll

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, AppDispatch } from '../services/store';
 import { fetchUsers } from '../services/usersSlice';
@@ -7,15 +7,23 @@ const UserList: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
   const users = useSelector((state: RootState) => state.users.users);
   const status = useSelector((state: RootState) => state.users.status);
+  const lastVisible = useSelector((state: RootState) => state.users.lastVisible);
 
   useEffect(() => {
     if (status === 'idle') {
-      dispatch(fetchUsers());
+      dispatch(fetchUsers({ lastVisible: null, limitCount: 10}));
     }
   }, [status, dispatch]);
 
+  const handleScroll = useCallback((e: React.UIEvent<HTMLDivElement>) => {
+    const bottom = e.currentTarget.scrollHeight - e.currentTarget.scrollTop === e.currentTarget.clientHeight;
+    if (bottom && status !== 'loading') {
+      dispatch(fetchUsers({ lastVisible, limitCount: 10}));
+    }
+  }, [dispatch, lastVisible, status]);
+
   return (
-    <>
+    <div onScroll={handleScroll} className="h-[90vh] overflow-auto">
       {users.map((user) => (
         <div key={user.id} className="flex items-center border border-b-gray-300 rounded p-2 space-x-2">
           <img src={user.image} alt={user.name} className="w-8 h-8 object-cover rounded-full" />
@@ -25,8 +33,7 @@ const UserList: React.FC = () => {
           </div>
         </div>
       ))}
-    </>
-
+    </div>
   );
 };
 
--- src/services/usersSlice.ts
+++ src/services/usersSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { db } from './firebase';
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query, startAfter, limit } from 'firebase/firestore';
 
 interface User {
   id: string;
@@ -13,21 +13,27 @@ interface User {
 interface UsersState {
   users: User[];
   status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  lastVisible: any; 
 }
 
 const initialState: UsersState = {
   users: [],
   status: 'idle',
+  lastVisible: null,
 };
 
-export const fetchUsers: any = createAsyncThunk('users/fetchUsers', async () => {
-  const q = query(collection(db, 'usersdata'), orderBy('timestamp'));
+export const fetchUsers: any = createAsyncThunk('users/fetchUsers', async ({ lastVisible, limitCount }: { lastVisible: any, limitCount: number }) => {
+  let q = query(collection(db, 'usersdata'), orderBy('timestamp'), limit(limitCount));
+  if (lastVisible) {
+    q = query(collection(db, 'usersdata'), orderBy('timestamp'), startAfter(lastVisible), limit(limitCount));
+  }
   const querySnapshot = await getDocs(q);
   const usersData = querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   })) as User[];
-  return usersData;
+  const lastVisibleDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+  return { usersData, lastVisibleDoc };
 });
 
 const usersSlice = createSlice({
@@ -45,7 +51,11 @@ const usersSlice = createSlice({
       })
       .addCase(fetchUsers.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.users = action.payload;
+        const newUsers = action.payload.usersData.filter(
+          (newUser: { id: string; }) => !state.users.some((existingUser) => existingUser.id === newUser.id)
+        );
+        state.users = [...state.users, ...newUsers];
+        state.lastVisible = action.payload.lastVisibleDoc;
       })
       .addCase(fetchUsers.rejected, (state) => {
         state.status = 'failed';
